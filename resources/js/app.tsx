1e80fb613763948a25484cc8f97dff9558b7d317
Pablo Cesar Vega Valverde <pvegav@autonoma.edu.pe>
2025-07-06 09:13:07 -0500

    [UPDATE] Implementar @tanstack/react-query

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,24 +1,39 @@
-import '../css/app.css';
+import '../css/app.css'
 
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createRoot } from 'react-dom/client';
-import { initializeTheme } from './hooks/use-appearance';
+import { createInertiaApp } from '@inertiajs/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import { createRoot } from 'react-dom/client'
+import { Toaster } from './components/ui/sonner'
+import { initializeTheme } from './hooks/use-appearance'
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
-    setup({ el, App, props }) {
-        const root = createRoot(el);
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
+  setup({ el, App, props }) {
+    const root = createRoot(el)
 
-        root.render(<App {...props} />);
-    },
-    progress: {
-        color: '#4B5563',
-    },
-});
+    root.render(
+      <QueryClientProvider client={queryClient}>
+        <App {...props} />
+        <Toaster />
+      </QueryClientProvider>
+    )
+  },
+  progress: {
+    color: '#4B5563'
+  }
+})
 
 // This will set light / dark mode on load...
-initializeTheme();
+initializeTheme()
