22eefd6ba4f9d1061db3cec25946b96be0125756
Pablo Cesar Vega Valverde <pvegav@autonoma.edu.pe>
2025-09-07 00:35:31 -0500

    [UPDATE] Ajustar filtro y imagenes

--- resources/js/pages/admin/avatars/index.tsx
+++ resources/js/pages/admin/avatars/index.tsx
@@ -1,8 +1,13 @@
-import { PageProps } from '@/types'
+import { Avatar } from '@/types/avatar'
+import { PaginatedResponse, ResourcePageProps } from '@/types/core'
 import AvatarsPage from './components'
 
-export default function TeacherAvatarsPage({ auth }: PageProps) {
-  return <AvatarsPage auth={auth} />
+type PageProps = Omit<ResourcePageProps<Avatar>, 'data'> & {
+  avatars: PaginatedResponse<Avatar>
+}
+
+export default function TeacherAvatarsPage({ avatars }: PageProps) {
+  return <AvatarsPage avatars={avatars} />
 }
 
 // Add a display name for the component for better debugging
