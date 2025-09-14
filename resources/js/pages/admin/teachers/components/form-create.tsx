c8f133dca549364fa9b239cf5be0cac455c27af4
Pablo Cesar Vega Valverde <pvegav@autonoma.edu.pe>
2025-09-14 07:36:08 -0500

    [UPDATE] Usar usuario y no DNI

--- resources/js/pages/admin/teachers/components/form-create.tsx
+++ resources/js/pages/admin/teachers/components/form-create.tsx
@@ -63,7 +63,7 @@ export function CreateTeacherDialog({ isOpen, onOpenChange }: CreateTeacherDialo
           {/* USUARIO */}
           <div className='grid grid-cols-2 gap-2'>
             <div className='flex flex-col gap-2'>
-              <Label htmlFor='name'>DNI (usuario)</Label>
+              <Label htmlFor='name'>Usuario</Label>
               <Input id='name' name='name' type='text' value={data.name} onChange={(e) => setData('name', e.target.value)} />
               <InputError message={errors.name} />
             </div>
